75a628beec9f81ae2370a156ec82ff048ee48d2f
Santosh Thoduka <santosh.thoduka@gmail.com>
2017-11-28 12:45:02 +0100

    add zactor to receive zyre messages and parse RobotPose2D messages

--- include/ccu_manager.hpp
+++ include/ccu_manager.hpp
@@ -14,16 +14,53 @@ class CCUManager
 public:
     CCUManager(ConfigParams config_params);
     ~CCUManager();
+    /*
+     * shouts a 'GOTO' command
+     * waypoint_id: name of semantic location on the map
+     */
     bool sendGOTOCommand(const std::string &waypoint_id);
+
+    /*
+     * shouts an elevator command
+     * elevator_command:
+     *     ENTER_ELEVATOR
+     *     EXIT_ELEVATOR
+     */
     bool sendElevatorCommand(const std::string &elevator_command);
+
+    /*
+     * shouts a coordination command
+     * coordination_command:
+     *      PAUSE
+     *      RESUME
+     */
     bool sendCoordinationCommand(const std::string &coordination_command);
+
+    /*
+     * Parses zeromq messages passed as JSON strings
+     *
+     * msg: message shouted by other nodes as JSON string
+     *
+     * this function has ownership over msg, so it
+     * deletes it after parsing
+     */
+    void parseInputMessage(zmsg_t *msg);
+
+    /*
+     * Returns zyre node
+     */
+    zyre::node_t * getNode() {return ccu_node_;}
 
 private:
     Json::Value getHeader(const std::string &command);
     void shout(const Json::Value &root);
 
-private:
     zmsg_t* string_to_zmsg(std::string msg);
+
+    void parseRobotPoseMessage(const Json::Value &root);
+    void parseProgressMessage(const Json::Value &root);
+
+private:
 
     ConfigParams config_params_;
 
@@ -31,6 +68,7 @@ private:
     std::string elevator_id_;
 
     zyre::node_t *ccu_node_;
+    zactor_t *actor_;
 
     Json::StreamWriterBuilder json_stream_builder_;
 };
