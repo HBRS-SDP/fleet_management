a195f928cf81539e73694b31d35cee79f7043cd6
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-06-21 23:51:47 +0200

    Added docstrings to the ccu store methods and started implementing the stubs

--- include/db/ccu_store.hpp
+++ include/db/ccu_store.hpp
@@ -23,10 +23,37 @@ public:
     CCUStore(std::string db_name);
     ~CCUStore() { }
 
+    /**
+     * Saves the given task to a database as a new document under the "tasks" collection
+     *
+     * @param task a reference to a const task::Task object representing a task
+     */
     void addTask(const task::Task& task);
+
+    /**
+     * Saves the given task id to a database as a new document under the "ongoing_tasks" collection
+     *
+     * @param task_id an integer representing the id of an already scheduled task
+     */
     void addOngoingTask(int task_id);
+
+    /**
+     * Returns a vector of ids representing all tasks that are saved
+     * under the "ongoing_tasks" collection
+     */
     std::vector<int> getOngoingTasks();
+
+    /**
+     * Returns a task::Task object representing the task with the given id
+     *
+     * @param task_id an integer representing the id of a task
+     */
     task::Task getTask(int task_id);
+
+    /**
+     * Returns a vector of task::Task object representing the scheduled tasks
+     * that are saved under the "tasks" collection
+     */
     std::vector<task::Task> getScheduledTasks();
 
 private:
--- src/db/ccu_store.cpp
+++ src/db/ccu_store.cpp
@@ -4,6 +4,11 @@ CCUStore::CCUStore(std::string db_name)
     : db_name_(db_name), db_instance_{}
 { }
 
+/**
+ * Saves the given task to a database as a new document under the "tasks" collection
+ *
+ * @param task a reference to a const task::Task object representing a task
+ */
 void CCUStore::addTask(const task::Task& task)
 {
     mongocxx::client db_client{mongocxx::uri{}};
@@ -16,6 +21,11 @@ void CCUStore::addTask(const task::Task& task)
     collection.insert_one(document.view());
 }
 
+/**
+ * Saves the given task id to a database as a new document under the "ongoing_tasks" collection
+ *
+ * @param task_id an integer representing the id of an already scheduled task
+ */
 void CCUStore::addOngoingTask(int task_id)
 {
     mongocxx::client db_client{mongocxx::uri{}};
@@ -26,16 +36,51 @@ void CCUStore::addOngoingTask(int task_id)
     collection.insert_one(document.view());
 }
 
+/**
+ * Returns a vector of ids representing all tasks that are saved
+ * under the "ongoing_tasks" collection
+ */
 std::vector<int> CCUStore::getOngoingTasks()
 {
-    return std::vector<int>();
+    mongocxx::client db_client{mongocxx::uri{}};
+    auto database = db_client[db_name_];
+    auto collection = database["ongoing_tasks"];
+    auto cursor = collection.find({});
+
+    std::vector<int> task_ids;
+    for (auto doc : cursor)
+    {
+        int task_id = doc["task_id"].get_int32();
+        task_ids.push_back(task_id);
+    }
+    return task_ids;
 }
 
+/**
+ * Returns a task::Task object representing the task with the given id
+ *
+ * @param task_id an integer representing the id of a task
+ */
 task::Task CCUStore::getTask(int task_id)
 {
-    return task::Task();
+    mongocxx::client db_client{mongocxx::uri{}};
+    auto database = db_client[db_name_];
+    auto collection = database["ongoing_tasks"];
+    auto doc = collection.find_one(bsoncxx::builder::stream::document{}
+                                   << "id" << task_id
+                                   << bsoncxx::builder::stream::finalize);
+    auto document_view = (*doc).view();
+
+    task::Task task;
+    task.id = document_view["id"].get_int32();
+
+    return task;
 }
 
+/**
+ * Returns a vector of task::Task object representing the scheduled tasks
+ * that are saved under the "tasks" collection
+ */
 std::vector<task::Task> CCUStore::getScheduledTasks()
 {
     return std::vector<task::Task>();
