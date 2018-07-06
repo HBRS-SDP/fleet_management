8be8172458ce6146f087a7fbc7370dd68c92a8d8
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-07-06 18:20:51 +0200

    Added a 'test' directory and a simple task dispatch unit test

---
+++ test/src/ccu_test.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+
+#include "data_structures/task.hpp"
+#include "task_manager.hpp"
+#include "config/config_params.hpp"
+#include "config/config_file_reader.hpp"
+
+void sendTask(task::TaskManager& task_manager)
+{
+    task::Task task;
+    task.id = 1;
+    task.start_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::system_clock::now().time_since_epoch()
+    ).count();
+
+    task.team_robot_ids = { "ropod_0" };
+
+    task::Action go_to_action;
+    go_to_action.id = "go_to_test";
+    go_to_action.eta = -1.0f;
+    go_to_action.execution_status = "in_progress";
+    task.robot_actions["ropod_0"].push_back(go_to_action);
+
+    task_manager.dispatchTask(task);
+}
+
+int main()
+{
+    ConfigParams config_params = ConfigFileReader::load("../../config/ccu_config.yaml");
+    task::TaskManager task_manager(config_params);
+
+    sendTask(task_manager);
+    return 0;
+}
