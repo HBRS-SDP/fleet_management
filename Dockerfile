8e69612d994412abbda2f0f0c5e9e6fca1acf3cf
Argentina Ortega Sainz <argentina.ortega@h-brs.de>
2018-08-24 19:38:46 +0200

    Update docker image

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM git.ropod.org:4567/ropod/ropod_common:latest
 
 WORKDIR /fleet-management
 ADD . /fleet-management/
-RUN pip3 install -r requirements.txt
+RUN pip3 install -r requirements.txt && pip3 install -e .
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='fleet_management_system',
+      version='0.1.0',
+      description='ROPOD Fleet Management System',
+      package_dir={'': '.'}
+      )
