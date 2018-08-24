560dbd64330de435ee3f96295418798d3a5c52cd
Alex Mitrevski <aleksandar.mitrevski@h-brs.de>
2018-08-24 20:12:48 +0200

    [test/task_request_test] Changed the pickup and delivery location names to actual OSM poses

--- fleet_management/test/task_request_test.py
+++ fleet_management/test/task_request_test.py
@@ -18,11 +18,11 @@ if __name__ == '__main__':
     task_request_msg['payload']['userId'] = '1'
     task_request_msg['payload']['deviceType'] = 'mobidik'
     task_request_msg['payload']['deviceId'] = '4800001663'
-    task_request_msg['payload']['pickupLocation'] = 'pickup_location'
-    task_request_msg['payload']['deliveryLocation'] = 'delivery_location'
-    task_request_msg['payload']['pickupLocationLevel'] = 0
-    task_request_msg['payload']['deliveryLocationLevel'] = 0
-    task_request_msg['payload']['startTime'] = int(round(time.time()) * 1000) + 15
+    task_request_msg['payload']['pickupLocation'] = 'AMK_B_L-1_C41_LA2'
+    task_request_msg['payload']['deliveryLocation'] = 'AMK_B_L4_C1_LA2'
+    task_request_msg['payload']['pickupLocationLevel'] = -1
+    task_request_msg['payload']['deliveryLocationLevel'] = 4
+    task_request_msg['payload']['startTime'] = int(round(time.time()) * 1000) + 10
 
     pyre_communicator = PyreBaseCommunicator('task_request_test', ['ROPOD'], [], verbose=True)
     pyre_communicator.shout(task_request_msg)
