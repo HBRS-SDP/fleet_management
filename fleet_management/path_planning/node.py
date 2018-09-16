466368abedf7cee14366a063b07de3df9025e669
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2018-09-16 13:25:29 +0200

    Fix indentation issues in path planner

--- fleet_management/path_planning/node.py
+++ fleet_management/path_planning/node.py
@@ -1,30 +1,31 @@
 from .visited_node import VisitedNode
 
+
 class Node():
-	def __init__(self, elm):
-		self.element = elm
+    def __init__(self, elm):
+        self.element = elm
 
-	@property
-	def id(self):
-		return self.element.get('id')
+    @property
+    def id(self):
+        return self.element.get('id')
 
-	@property
-	def lat(self):
-		return float(self.element.get('lat'))
+    @property
+    def lat(self):
+        return float(self.element.get('lat'))
 
-	@property
-	def lon(self):
-		return float(self.element.get('lon'))
+    @property
+    def lon(self):
+        return float(self.element.get('lon'))
 
-	def __eq__(self, other):
-		if isinstance(other, VisitedNode):
-			return self.id == other.node.id
-		if isinstance(other, (Node)):
-			return self.id == other.id
+    def __eq__(self, other):
+        if isinstance(other, VisitedNode):
+            return self.id == other.node.id
+        if isinstance(other, (Node)):
+            return self.id == other.id
 
-	def __repr__(self):
-		return "<Node id=%(id)s, lat=%(lat)s, lon=%(lon)s>" % {
-				'id': self.id,
-				'lat': self.lat,
-				'lon': self.lon,
-		}
+    def __repr__(self):
+        return "<Node id=%(id)s, lat=%(lat)s, lon=%(lon)s>" % {
+            'id': self.id,
+            'lat': self.lat,
+            'lon': self.lon,
+        }
