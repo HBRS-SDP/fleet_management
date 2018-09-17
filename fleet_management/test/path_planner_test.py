07f247d132ebff33eb172c934d3e191678d68844
Lakshadeep Naik <lakshadeep.naik@gmail.com>
2018-09-17 11:05:07 +0200

    Fixed path planner test

--- fleet_management/test/path_planner_test.py
+++ fleet_management/test/path_planner_test.py
@@ -3,15 +3,14 @@ from fleet_management.path_planner import PathPlanner
 from fleet_management.structs.area import Area
 
 if __name__ == '__main__':
-    start = Area()
-    start.name = 'AMK_D_L-1_C41_LA1'
+    planner = PathPlanner("127.0.0.1:8000")
+    start = planner.get_area('AMK_D_L-1_C41_LA1')
     start.floor_number = -1
 
-    destination = Area()
-    destination.name = 'AMK_A_L-1_RoomBU21_LA1'
+    destination = planner.get_area('AMK_A_L-1_RoomBU21_LA1')
     destination.floor_number = -1
 
     print('Generating plan from', start.name, " to ", destination.name)
-    planner = PathPlanner("127.0.0.1:8000")
+    
     plan = planner.get_path_plan(start, destination)
     print(plan)
