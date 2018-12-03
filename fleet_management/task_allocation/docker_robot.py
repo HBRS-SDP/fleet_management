dd3e5a47673c7ff1b337074aa2bede76699bedcd
Argentina Ortega Sainz <argentina.ortega@h-brs.de>
2018-12-03 14:32:15 +0100

    [task allocation] Fix robot system exit

--- fleet_management/task_allocation/docker_robot.py
+++ fleet_management/task_allocation/docker_robot.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from fleet_management.config.config_file_reader import ConfigFileReader
 from fleet_management.task_allocation import Robot
 from fleet_management.db.ccu_store import CCUStore
+import time
 
 import argparse
 
@@ -15,4 +16,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
     ropod_id = args.ropod_id
 
-    Robot(ropod_id, config_params, ccu_store, verbose_mrta=True)
+    robot = Robot(ropod_id, config_params, ccu_store, verbose_mrta=True)
+
+    try:
+        while True:
+            time.sleep(0.5)
+        raise KeyboardInterrupt
+    except (KeyboardInterrupt, SystemExit):
+        print("Terminating mock robot...")
+        robot.shutdown()
+        print("Exiting...")
