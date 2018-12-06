9cc1c5d954c3fd7969b19bc2f77158f01457924c
Argentina Ortega Sainz <argentina.ortega@h-brs.de>
2018-12-06 10:25:37 +0100

    [gitlab-ci] Increasing wait time before elevator tests

--- fleet_management/test/elevator_request_test.py
+++ fleet_management/test/elevator_request_test.py
@@ -51,7 +51,7 @@ class ElevatorRequester(PyreBaseCommunicator):
 if __name__ == '__main__':
     test = ElevatorRequester()
     try:
-        time.sleep(14)
+        time.sleep(20)
         test.send_request()
         while not test.terminated:
             time.sleep(0.5)
