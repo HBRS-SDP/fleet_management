8299ea81803128f306a9d51a2b80972fcf17f979
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-03-12 15:28:38 +0100

    Fix yaml loading warning

--- fleet_management/config/config_file_reader.py
+++ fleet_management/config/config_file_reader.py
@@ -123,6 +123,6 @@ class ConfigFileReader(object):
     @staticmethod
     def __read_yaml_file(config_file_name):
         file_handle = open(config_file_name, 'r')
-        data = yaml.load(file_handle)
+        data = yaml.safe_load(file_handle)
         file_handle.close()
         return data
