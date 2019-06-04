0a0158ea3114dea40e581225646c1196480495f8
Santosh Thoduka <santosh.thoduka@gmail.com>
2019-06-04 18:50:19 +0200

    add resource monitor mockup which replies to elevator call requests

---
+++ fleet_management/test/resource_monitor_mockup.py
@@ -0,0 +1,37 @@
+from __future__ import print_function
+import time
+import json
+
+from ropod.pyre_communicator.base_class import RopodPyre
+from ropod.utils.uuid import generate_uuid
+from ropod.utils.timestamp import TimeStamp as ts
+
+
+class ResourceMonitorMockup(RopodPyre):
+    def __init__(self):
+        super().__init__('resource_monitor_mockup', ['ROPOD'], [], verbose=False)
+
+    def receive_msg_cb(self, msg_content):
+        message = self.convert_zyre_msg_to_dict(msg_content)
+        if message is None:
+            return
+
+        if message['header']['type'] == 'ROBOT-ELEVATOR-CALL-REQUEST':
+            with open('config/msgs/elevator/ropod-elevator-call-reply.json') as msg_file:
+                elevator_reply_msg = json.load(msg_file)
+                elevator_reply_msg['header']['msgId'] = generate_uuid()
+                elevator_reply_msg['header']['timestamp'] = ts.get_time_stamp()
+                elevator_reply_msg['payload']['queryId'] = message['payload']['queryId']
+                self.shout(elevator_reply_msg, "ROPOD")
+
+if __name__ == '__main__':
+    mockup = ResourceMonitorMockup()
+    mockup.start()
+
+    try:
+        while not mockup.terminated:
+            time.sleep(0.5)
+        raise KeyboardInterrupt
+    except (KeyboardInterrupt, SystemExit):
+        print("Exiting mockup...")
+        mockup.shutdown()
