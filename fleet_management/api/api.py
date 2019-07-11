576135cec410d2b558808fda9b652b25f6c11918
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-07-11 22:17:59 +0200

    api: Reorganize order of initialisation for api components

--- fleet_management/api/api.py
+++ fleet_management/api/api.py
@@ -8,18 +8,29 @@ from fleet_management.api.zyre import FMSZyreAPI
 class API(object):
     def __init__(self, config):
         self.logger = logging.getLogger('fms.api')
+
         self.publish_dict = dict()
+        self.__configure(config)
+
+        self.logger.debug("Configuring Zyre interface")
         self.zyre = API.get_zyre_api(config.get('zyre').get('zyre_node', None))
-        self.ros = API.get_ros_api('test')
+
         rest_config = {
             'bind': '%s:%s' % ('127.0.0.1', '8080'),
             'workers': 1,
         }
+        self.logger.debug("Configuring REST interface")
         self.rest = API.get_rest_api(rest_config)
-        self.__configure(config)
+
+        self.logger.debug("Configuring ROS interface")
+        self.ros = API.get_ros_api('test')
+
+
+        self.logger.info("Initialized API")
 
     def publish(self, msg, **kwargs):
         msg_type = msg.get('header').get('type')
+        self.logger.debug("Publishing message of type %s", msg_type)
 
         if self.zyre:
             method = self.publish_dict.get(msg_type.lower()).get('method').get('zyre')
@@ -27,20 +38,18 @@ class API(object):
 
     def __configure(self, config_params):
         self.publish_dict.update(config_params.get('publish'))
+        self.logger.debug("Publish dictionary: %s", self.publish_dict)
 
     @staticmethod
     def get_zyre_api(zyre_config):
-        logging.info("Configuring Zyre interface")
         zyre_api = FMSZyreAPI(zyre_config)
         return zyre_api
 
     @staticmethod
     def get_ros_api(ros_config):
-        logging.info("Configuring ROS interface")
         return ROSInterface(ros_config)
 
     @staticmethod
     def get_rest_api(rest_config):
-        logging.info("Configuring REST interface")
         return RESTInterface(rest_config)
 
