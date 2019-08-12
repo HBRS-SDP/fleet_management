46f15da905f798bcc7e47d10be0096b0ca5e3bbf
Angela Enriquez <anenriqu@outlook.com>
2019-08-12 14:26:13 +0200

    the while loop runs in the robot proxy

--- fleet_management/task_allocation/robot.py
+++ fleet_management/task_allocation/robot.py
@@ -1,7 +1,6 @@
 from fleet_management.config.loader import Config
 import argparse
 import time
-import logging
 
 if __name__ == '__main__':
 
@@ -14,17 +13,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     robot_id = args.robot_id
 
-    robot = config.configure_robot_proxy(robot_id, ccu_store)
+    robot_proxy = config.configure_robot_proxy(robot_id, ccu_store)
 
     time.sleep(5)
 
-    robot.api.start()
-
-    try:
-        while True:
-            robot.api.run()
-            time.sleep(0.5)
-    except (KeyboardInterrupt, SystemExit):
-        logging.info("Terminating %s proxy ...", robot_id)
-        robot.api.shutdown()
-        logging.info("Exiting...")
+    robot_proxy.run()
