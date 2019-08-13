9676b0ea1c4cfc5af16cf0501cdae31bd336a5b1
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-08-13 12:32:36 +0200

    test/resource_manager: Use the right config loader

--- fleet_management/test/resource_manager_test.py
+++ fleet_management/test/resource_manager_test.py
@@ -1,17 +1,14 @@
 from __future__ import print_function
 
-import os.path
 import time
 import unittest
 
-from OBL import OSMBridge
 from ropod.pyre_communicator.base_class import RopodPyre
 from ropod.utils.models import MessageFactory
 from ropod.utils.uuid import generate_uuid
 
-from fleet_management.config.config_file_reader import ConfigFileReader
+from fleet_management.config.loader import Config
 from fleet_management.db.ccu_store import CCUStore
-from fleet_management.resource_manager import ResourceManager
 
 
 class QueryTest(RopodPyre):
@@ -39,21 +36,17 @@ class QueryTest(RopodPyre):
 
         self.response = message
 
+
 class ResourceManagerTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        test_dir = os.path.abspath(os.path.dirname(__file__))
-        code_dir = os.path.abspath(os.path.join(test_dir, '..'))
-        main_dir = os.path.dirname(code_dir)
-        config_file = os.path.join(main_dir, "config/ccu_config.yaml")
-        config_params = ConfigFileReader.load(config_file)
+        config = Config(initialize=False)
+
         cls.ccu_store = CCUStore('sub_area_management_test')
-        cls.osm_bridge = OSMBridge(
-            server_ip=config_params.overpass_server.ip,
-            server_port=config_params.overpass_server.port)
-        cls.resource_manager = ResourceManager(
-            config_params, cls.ccu_store, cls.osm_bridge)
+        cls.osm_bridge = config.configure_osm_bridge()
+        cls.resource_manager = config.configure_resource_manager(cls.ccu_store)
+        cls.resource_manager.add_plugin('osm_bridge', cls.osm_bridge)
         cls.test_pyre_node = QueryTest()
         cls.timeout_duration = 3
         time.sleep(3)
@@ -90,6 +83,7 @@ class ResourceManagerTest(unittest.TestCase):
     #    self.assertEqual(message['header']['type'], msg_type)
     #    self.assertIn('payload', message)
     #    #TODO: a bunch of more assert statements
+
 
 if __name__ == '__main__':
     unittest.main()
