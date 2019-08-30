f6e44ac8400169134fcc6cd213aac97f6581ab0f
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-08-30 15:37:17 +0200

    utils: Add message to document converter

--- fleet_management/utils/messages.py
+++ fleet_management/utils/messages.py
@@ -3,6 +3,21 @@ import inflection
 
 # Inspired by https://realpython.com/inheritance-composition-python/
 
+class Message:
+
+    @classmethod
+    def from_dict(cls, msg):
+        return {inflection.camelize(prop, False): cls._format_dict(value)
+                for prop, value in msg.items()}
+
+    @classmethod
+    def _format_dict(cls, value):
+        if isinstance(value, dict):
+            return cls.from_dict(value)
+        else:
+            return value
+
+
 class Document(dict):
 
     @classmethod
