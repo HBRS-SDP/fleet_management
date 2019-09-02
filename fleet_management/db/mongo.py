89568b7c64c792721d892e02b991cb63551fca5d
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-09-02 18:53:32 +0200

    db: Add builder for MongoStore

--- fleet_management/db/mongo.py
+++ fleet_management/db/mongo.py
@@ -70,3 +70,17 @@ class MongoStoreInterface:
 
     def clean(self):
         pass
+
+
+class MongoStoreBuilder:
+    def __init__(self):
+        self._instance = None
+
+    def __call__(self, db_name, port, **_):
+        if not self._instance:
+            store = MongoStore(db_name, port)
+            self._instance = MongoStoreInterface(store)
+        return self._instance
+
+
+Store = MongoStoreBuilder()
