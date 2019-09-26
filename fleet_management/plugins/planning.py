4d82664ae913a3975bd6743326f26ae0da79fd67
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-09-26 10:41:01 +0200

    plugins: Move kb initialization to planning module

--- fleet_management/plugins/planning.py
+++ fleet_management/plugins/planning.py
@@ -1,7 +1,6 @@
 import logging
 import uuid
 
-from fleet_management.db.init_db import initialize_knowledge_base
 from fleet_management.db.models import actions
 from fleet_management.exceptions.osm import OSMPlannerException
 from fmlib.models.tasks import TaskPlan
@@ -235,3 +234,22 @@ class TaskPlannerInterface(object):
                         self.logger.debug(sub_area.name)
 
         return task_plan_with_paths
+
+
+def initialize_knowledge_base(kb_database_name):
+    kb_interface = KnowledgeBaseInterface(kb_database_name)
+
+    print('[initialize_knowledge_base] Initializing elevators')
+    # TODO: Use the actual areas where the elevators are
+    elevator_facts = [('elevator_at', [('elevator', 'elevator0'),
+                                       ('loc', 'AMK_B_L-1_C2')]),
+                      ('elevator_at', [('elevator', 'elevator0'),
+                                       ('loc', 'AMK_B_L4_C0')])]
+    kb_interface.insert_facts(elevator_facts)
+
+    elevator_fluents = [('elevator_floor', [('elevator', 'elevator0')], 100)]
+    kb_interface.insert_fluents(elevator_fluents)
+
+    elevator_location_fluents = [('location_floor', [('loc', 'AMK_B_L-1_C2')], -1),
+                                 ('location_floor', [('loc', 'AMK_B_L4_C0')], 4)]
+    kb_interface.insert_fluents(elevator_location_fluents)
