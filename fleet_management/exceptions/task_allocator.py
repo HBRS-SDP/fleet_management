f56b12b19785d963cb2a3c6254c0223c2354a038
Angela Enriquez <anenriqu@outlook.com>
2019-09-26 10:25:43 +0200

    exceptions/task_allocator.py: Remove unused exception

--- fleet_management/exceptions/task_allocator.py
+++
@@ -1,8 +0,0 @@
-class UnsuccessfulAllocationAlternativeTimeSlot(Exception):
-    """ Raised when a task could not be allocated at the desired time slot.
-    An alternative timeslot for the task is passed as an argument
-     @param alternative_timeslots: dict (keys = task_ids, values = robot_id, start_time """
-
-    def __init__(self, alternative_timeslots):
-        Exception.__init__(self, alternative_timeslots)
-        self.alternative_timeslots = alternative_timeslots
