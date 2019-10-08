dd2dd9b18e0570a06baad04f5ce26953f2e57eca
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-10-08 17:34:47 +0200

    models: Move robot model ropod-independent code to fmlib

--- fleet_management/db/models/robot.py
+++ fleet_management/db/models/robot.py
@@ -1,32 +1,33 @@
 from fleet_management.db.models.environment import Position
-from fmlib.models.robot import HardwareComponent, Robot, RobotHardware, SoftwareComponent, SoftwareStack
+from fmlib.models import robot as models
+from fmlib.models.robot import Availability, ComponentStatus, RobotStatus
 from pymodm import EmbeddedMongoModel, fields, MongoModel
 
 
-class SensorCube(HardwareComponent):
+class SensorCube(models.HardwareComponent):
     pass
 
 
-class DockingMechanism(HardwareComponent):
+class DockingMechanism(models.HardwareComponent):
     pass
 
 
-class Platform(RobotHardware):
+class Platform(models.RobotHardware):
     docking_mechanism = fields.EmbeddedDocumentField(DockingMechanism)
     sensor_cubes = fields.EmbeddedDocumentListField(SensorCube)
 
 
-class RopodSoftwareStack(SoftwareStack):
-    diagnosis = fields.EmbeddedDocumentListField(SoftwareComponent)
-    communication = fields.EmbeddedDocumentListField(SoftwareComponent)
-    execution = fields.EmbeddedDocumentListField(SoftwareComponent)
+class RopodSoftwareStack(models.SoftwareStack):
+    diagnosis = fields.EmbeddedDocumentListField(models.SoftwareComponent)
+    communication = fields.EmbeddedDocumentListField(models.SoftwareComponent)
+    execution = fields.EmbeddedDocumentListField(models.SoftwareComponent)
 
 
 class BlackBoxModel(MongoModel):
     black_box_id = fields.CharField(primary_key=True)
-    version = fields.EmbeddedDocumentField(HardwareComponent)
-    logger = fields.EmbeddedDocumentField(SoftwareComponent)
-    fault_detection = fields.EmbeddedDocumentField(SoftwareComponent)
+    version = fields.EmbeddedDocumentField(models.HardwareComponent)
+    logger = fields.EmbeddedDocumentField(models.SoftwareComponent)
+    fault_detection = fields.EmbeddedDocumentField(models.SoftwareComponent)
 
 
 class RopodVersion(EmbeddedMongoModel):
@@ -35,7 +36,29 @@ class RopodVersion(EmbeddedMongoModel):
     software = fields.EmbeddedDocumentField(RopodSoftwareStack)
 
 
-class Ropod(Robot):
+class Ropod(models.Robot):
     nickname = fields.CharField(default=None)
     version = fields.EmbeddedDocumentField(RopodVersion)
     position = fields.EmbeddedDocumentField(Position)
+
+    @classmethod
+    def create_new(cls, robot_id, **kwargs):
+        robot = cls(robot_id, **kwargs)
+        robot.position = Position()
+
+        component_status = ComponentStatus()
+        availability = Availability()
+        hw_version = Platform()
+        sw_version = RopodSoftwareStack()
+        black_box = BlackBoxModel('black_box_001')  # TODO This shouldn't be hardcoded
+
+        robot.status = RobotStatus(availability=availability, component_status=component_status)
+        robot.version = RopodVersion(hardware=hw_version, software=sw_version, black_box=black_box)
+
+        robot.save()
+
+        return robot
+
+    def update_position(self, **kwargs):
+        self.position.update_position(**kwargs)
+        self.save()
