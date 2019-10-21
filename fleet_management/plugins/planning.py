136f35f112b1b55cb8644124badf8fca8987c8c7
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-10-21 17:23:30 +0200

    plugins: Initialize knowledge base for BRSU map

--- fleet_management/plugins/planning.py
+++ fleet_management/plugins/planning.py
@@ -241,14 +241,14 @@ def initialize_knowledge_base(kb_database_name):
     print('[initialize_knowledge_base] Initializing elevators')
     # TODO: Use the actual areas where the elevators are
     elevator_facts = [('elevator_at', [('elevator', 'elevator0'),
-                                       ('loc', 'AMK_B_L-1_C2')]),
+                                       ('loc', 'BRSU_C_L0_C2')]),
                       ('elevator_at', [('elevator', 'elevator0'),
-                                       ('loc', 'AMK_B_L4_C0')])]
+                                       ('loc', 'BRSU_A_L2_C1')])]
     kb_interface.insert_facts(elevator_facts)
 
     elevator_fluents = [('elevator_floor', [('elevator', 'elevator0')], 100)]
     kb_interface.insert_fluents(elevator_fluents)
 
-    elevator_location_fluents = [('location_floor', [('loc', 'AMK_B_L-1_C2')], -1),
-                                 ('location_floor', [('loc', 'AMK_B_L4_C0')], 4)]
+    elevator_location_fluents = [('location_floor', [('loc', 'BRSU_C_L0_C2')], 0),
+                                 ('location_floor', [('loc', 'BRSU_A_L2_C1')], 2)]
     kb_interface.insert_fluents(elevator_location_fluents)
