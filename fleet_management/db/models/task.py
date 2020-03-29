e482f6490353450e3b76a02e710535a5c65f126a
Ángela Enríquez Gómez <anenriqu@outlook.com>
2020-03-29 13:29:48 +0200

    task: Add methods that use robot model
    The assigned_robots is a list of Ropod models and not a list of strings

--- fleet_management/db/models/task.py
+++ fleet_management/db/models/task.py
@@ -7,3 +7,26 @@ class TransportationTask(Task):
     assigned_robots = fields.EmbeddedDocumentListField(Ropod)
 
     objects = TaskManager()
+
+    def to_dict(self):
+        dict_repr = super().to_dict()
+        robots_dict = dict()
+        for robot in self.assigned_robots:
+            robots_dict[robot.robot_id] = robot.to_dict()
+        dict_repr["assigned_robots"] = robots_dict
+        return dict_repr
+
+    @classmethod
+    def get_tasks_by_robot(cls, robot_id):
+        return [task for task in cls.objects.all() if robot_id in [robot.robot_id for robot in task.assigned_robots]]
+
+    @classmethod
+    def get_tasks(cls, robot_id=None, status=None):
+        if status:
+            tasks = cls.get_tasks_by_status(status)
+        else:
+            tasks = cls.objects.all()
+
+        tasks_by_robot = [task for task in tasks if robot_id in [robot.robot_id for robot in task.assigned_robots]]
+
+        return tasks_by_robot
