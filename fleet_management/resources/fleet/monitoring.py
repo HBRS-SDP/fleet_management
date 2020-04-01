eca57b08a3a2e2c197b871fbd76e2347c31ea414
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2020-04-01 11:50:00 +0200

    fleet/monitoring: Add version update callback

--- fleet_management/resources/fleet/monitoring.py
+++ fleet_management/resources/fleet/monitoring.py
@@ -1,7 +1,8 @@
 import logging
 
 from fmlib.db.mongo import MongoStore, MongoStoreInterface
-from fleet_management.db.models.robot import Ropod as Robot
+from fmlib.models.robot import SoftwareComponent
+from fleet_management.db.models.robot import Ropod as Robot, RopodSoftwareStack
 
 
 class FleetMonitor:
@@ -42,6 +43,60 @@ class FleetMonitor:
 
     def __configure_api(self, api_config):
         self.api.register_callbacks(self, api_config)
+
+    def robot_version_cb(self, msg):
+        payload = msg.get('payload')
+        robot_id = payload.get('robotId')
+        robot = self.robots.get(robot_id)
+
+        software_ = self._process_wstool_msg(payload.get('softwareVersion'))
+
+        robot.update_version(software=software_,
+                             hardware=payload.get('hardwareVersion'))
+
+    def _process_wstool_msg(self, software):
+        sw_version = RopodSoftwareStack()
+        for component in software:
+            name = component.get('localname')
+            config_version = component.get('version')
+            actual_version = component.get('actualversion')
+            config_mismatch = component.get('specversion') != actual_version
+
+            update = component.get('remote_revision') != actual_version
+            modified = component.get('modified')
+            if not modified:
+                modified = False
+
+            # Create the component document
+            component_ = SoftwareComponent(name=name,
+                                           version=config_version,
+                                           version_uid=actual_version,
+                                           update_available=update,
+                                           config_mismatch=config_mismatch,
+                                           uncommitted_changes=modified)
+            # Categorize the entries
+            properties = component.get('properties')
+            if properties:
+                category = properties[0].get('meta', dict()).get('category', 'uncategorized')
+            else:
+                category = 'uncategorized'
+
+            if category == 'navigation':
+                sw_version.navigation_stack.append(component_)
+            elif category == 'diagnosis':
+                sw_version.diagnosis.append(component_)
+            elif category == 'communication':
+                sw_version.communication.append(component_)
+            elif category == 'execution':
+                sw_version.execution.append(component_)
+            elif category == 'world model':
+                sw_version.world_model.append(component_)
+            elif category == 'interfaces':
+                sw_version.interfaces.append(component_)
+            else:
+                sw_version.uncategorized.append(component_)
+
+        return sw_version
 
 
 if __name__ == '__main__':
