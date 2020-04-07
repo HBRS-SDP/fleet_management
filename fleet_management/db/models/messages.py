a6405bd317f27d9bbf89c4879f54f82c89072d76
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2020-04-07 18:29:46 +0200

    db: Add message model

---
+++ fleet_management/db/models/messages.py
@@ -0,0 +1,12 @@
+import datetime
+
+from pymodm import fields, MongoModel
+
+
+class Message(MongoModel):
+    msg_id = fields.UUIDField(primary_key=True)
+    timestamp = fields.DateTimeField(default=datetime.datetime.now())
+    metamodel = fields.CharField()
+    type = fields.CharField()
+    payload = fields.DictField()
+    receiver_ids = fields.ListField(blank=True)
